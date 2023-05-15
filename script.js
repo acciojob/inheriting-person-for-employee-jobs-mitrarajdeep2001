8a75b33e55a342e026dafaec9ad68706a7e0480d
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-05-15 12:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.Name}, I am ${this.Age} years old.`)
+	return `Hello, my name is ${this.Name}, I am ${this.Age} years old.`
 }
 
 function Employee(name, age, jobTitle) {
@@ -17,11 +17,11 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype)
 const E1 = new Employee('Rajdeep', '22', 'SDE')
 Employee.prototype.jobGreet = function () {
-	console.log(`Hello, my name is ${this.Name}, I am ${this.Age} years old, and my job title is ${this.JobTitle}.`)
+	return `Hello, my name is ${this.Name}, I am ${this.Age} years old, and my job title is ${this.JobTitle}.`
 }
 
-E1.greet()
-E1.jobGreet()
+console.log(E1.greet()) 
+console.log(E1.jobGreet()) 
 
 
 // Do not change code below this line
